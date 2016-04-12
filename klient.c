946e1a63e8608372e1884c87254443bf4c71312c
MGalaszewska <marti18.1993@gmail.com>
2016-04-12 10:30:15 +0200

    Update klient.c

--- klient.c
+++ klient.c
@@ -1,82 +1,33 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <fcntl.h>
-
 #include <string.h>
-
 #include <sys/stat.h>
-
 #include <unistd.h>
-
-
 
 #define MAX_BUF 4096
 
-
-
 int main(int argc, char* argv[])
-
 {
-
     int klient, serwer;
-
-    char klientfifo[] = "kfifo";
-
-    char serwerfifo[] = "sfifo";
-
-    char msg[80], buf[10];
-
+    char klientfifo[] = "klientfifo";
+    char serwerfifo[] = "serwerfifo";
+    char msg[20];
     struct stat st;
 
-
-
-    // if no fifos, create 'em
-
     if (stat(klientfifo, &st) != 0)
-
         mkfifo(klientfifo, 0666);
 
-    if (stat(serwerfifo, &st) != 0)
-
-        mkfifo(serwerfifo, 0666);
-
-
-
     serwer= open(serwerfifo, O_WRONLY);
-
     klient= open(klientfifo, O_RDONLY);
 
-
-
 int id;
-
 printf("Podaj liczbe calkowita:\n");
-
 scanf("%d", &id);
-
-        write(serwer, &id, strlen(msg)+1);
-
-char str[20];
-
-if(read(klient, &str, MAX_BUF) > 0) {
-
-	printf("%s", str);
-
+        write(serwer, &id, sizeof(id));
+if(read(klient, &msg, sizeof(msg)) > 0) {
+	printf("%s", msg);
 	close(klient);
-
 }
-
-    // delete fifos
-
-//    unlink(klientfifo);
-
-//    unlink(fifo_name2);
-
     return EXIT_SUCCESS;
-
 }
-
-
-
