e93c5b1ef0d16d05e9a319b8443f34359b368241
MGalaszewska <marti18.1993@gmail.com>
2016-04-12 10:30:35 +0200

    Update serwer.c

--- serwer.c
+++ serwer.c
@@ -4,55 +4,57 @@
 #include <unistd.h>
 #include <string.h>
 #include <signal.h>
+#include <sys/stat.h>
 
 #define MAX_BUF 4096
 
 int main(int argc, char* argv[])
 {
-    int klient, serwer;
-    int buf, i, x=0;
-    char num[3];
-    char nazw[20];
-
-    char klientfifo[] = "kfifo";
-    char serwerfifo[] = "sfifo";
-    serwer= open(serwerfifo, O_RDONLY);
-    klient= open(klientfifo, O_WRONLY);
+int klient, serwer;
+int buf, i, x=0;
+char num[3];
+char nazw[20];
+char klientfifo[] = "klientfifo";
+char serwerfifo[] = "serwerfifo";
+mkfifo(serwerfifo, 0666);
+serwer = open(serwerfifo, O_RDONLY);
+klient = open(klientfifo, O_WRONLY);
 
 typedef struct osoba {
 int id;
 char nazwisko[20];
 } osoba;
 
-osoba tab[20];
+osoba osoby[20];
 
 for(i=0; i<20; i++) {
-	tab[i].id = i+1;
+	osoby[i].id = i;
 	sprintf(num, "%d", i);
 	strncpy(nazw, "Nazwisko", 10);
 	strncat(nazw, num, 3);
-	strncpy(tab[i].nazwisko, nazw, 19);
-//	printf("%d: %s\n", tab[i].id, tab[i].nazwisko);
+	strncpy(osoby[i].nazwisko, nazw, 19);
 }
-    // receive messages
-    while (1)
+while (1)
     {
         if (read(serwer, &buf, MAX_BUF) > 0)
         {
 for(i=0; i<20; i++) {
-if(tab[i].id == buf) {
-x=0; break;}
-else {x=1;}
+if(osoby[i].id == buf) {
+x=0; break;
 }
-	int sid = fork();
+else {
+x=1;
+}
+}
+	int p = fork();
 		if(x==0) {
-		    write(klient, tab[buf].nazwisko, sizeof(tab[i].nazwisko));
-		    kill(sid, SIGKILL);
+		write(klient, osoby[buf].nazwisko, sizeof(osoby[i].nazwisko));
+		kill(p, SIGKILL);
         	}
 		else {
-		    write(klient, "nie znaleziono", sizeof("nie znaleziono"));
-		    kill(sid, SIGKILL);
+		write(klient, "Nie znaleziono", sizeof("Nie znaleziono"));
+		kill(p, SIGKILL);
 		}
-	}
-    }
+}
+}
 }
